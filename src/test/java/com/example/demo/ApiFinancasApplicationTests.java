075e2bfc6d871fed18861ea7e1f1a65a9566df1a
samuelmsilva2v <samuelmsilva@outlook.com.br>
2024-12-19 13:40:51 -0300

    app properties prod

--- src/test/java/com/example/demo/ApiFinancasApplicationTests.java
+++ src/test/java/com/example/demo/ApiFinancasApplicationTests.java
@@ -1,13 +1,64 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
+import java.util.UUID;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.example.demo.domain.models.dto.CategoriaRequestDto;
+import com.example.demo.domain.models.dto.CategoriaResponseDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.javafaker.Faker;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ApiFinancasApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	private static UUID categoriaId;
+	
 	@Test
-	void contextLoads() {
+	@Order(1)
+	void cadastrarCategoriaTest() throws Exception {
+		
+		var faker = new Faker(Locale.forLanguageTag("pt-BR"));
+		
+		var request = new CategoriaRequestDto();
+		request.setNome(faker.book().genre());
+		
+		var result = mockMvc.perform(post("/api/categorias")
+				.contentType("application/json")
+				.content(objectMapper.writeValueAsString(request)))
+				.andExpect(status().isOk())
+				.andReturn();
+		
+		var content = result.getResponse().getContentAsString(StandardCharsets.UTF_8);
+		
+		var response = objectMapper.readValue(content, CategoriaResponseDto.class);
+		
+		assertNotNull(response.getId());
+		assertEquals(response.getNome(), request.getNome());
+		
+		categoriaId = response.getId();
 	}
 
 }
